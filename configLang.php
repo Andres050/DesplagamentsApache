7d0dbeee345362974dcca781c58c239edaef807b
Andres Garcia Bauza <militaxx5@gmail.com>
2021-11-25 10:42:02 +0100

    da

--- configLang.php
+++ configLang.php
@@ -13,7 +13,8 @@ if (!isset($_GET['lang'])) {
             }
             $_SESSION['lang'] = "es";
         }
-} else if (isset($_GET['lang'])        && $_SESSION['lang'] != $_GET['lang'] && !empty($_GET['lang'])) {
+        }
+} else if (isset($_GET['lang']) && $_SESSION['lang'] != $_GET['lang'] && !empty($_GET['lang'])) {
         if ($_GET['lang'] == "es")
             $_SESSION['lang'] = "es";
         else if ($_GET['lang']  == "en")
@@ -24,7 +25,6 @@ if (!isset($_GET['lang'])) {
             $_SESSION['lang'] = "ja";
         else if ($_GET['lang']  == "fr")
             $_SESSION['lang'] = "fr";
-    }
 }
 
 
