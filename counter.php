c9f22c873266720f850bb59fa0143fc08a47a83a
Andres Garcia Bauza <militaxx5@gmail.com>
2021-11-25 12:39:26 +0100

    css archive

--- counter.php
+++ counter.php
@@ -15,7 +15,7 @@ setcookie('andres', $ncookie);
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css">
-  <link rel="stylesheet" href="/headerStyle.css">
+  <link rel="stylesheet" href="/css/headerStyle.css">
 </head>
 <style>
 button {
--- login.php
+++ login.php
@@ -5,8 +5,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css">
-  <link rel="stylesheet" href="/headerStyle.css">
-  <link rel="stylesheet" href="/login.css">
+  <link rel="stylesheet" href="/css/headerStyle.css">
+  <link rel="stylesheet" href="/css/login.css">
 </head>
 <style>
 button {
