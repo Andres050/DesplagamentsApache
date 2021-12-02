4739fb7c50c69bfe37b4bbf3fa9460e97ed39b14
Andres Garcia Bauza <militaxx5@gmail.com>
2021-12-02 16:34:48 +0100

    no

--- CapaNegoci/selectTraductions.php
+++ CapaNegoci/selectTraductions.php
@@ -7,13 +7,12 @@ $lenguaje  = $_SESSION['lang'];
 $resultado = "SELECT nombre_idioma , idPro , namePro , descripPro FROM $table";
 $tra = $conn->query($resultado);
 
-echo $resultado;
 if ($tra->num_rows > 0) {
     while($row = $tra->fetch_assoc()) {
         // CREO EL OBJETO PRODUCTO CON EL CONTENIDO DE CADA PRODUCTO!!!
         $producto = new Producto($row["idPro"],$row["namePro"],$row["descripPro"],null,null);
-        $ArrProductoTra = (array)$producto;
-        $ArrProductoTra = array_values($ArrProductoTra);
+        $ProductoTra = (array)$producto;
+        $ProductoTra = array_values($ArrProductoTra, $ProductoTra);
     }
 }
 ?>
