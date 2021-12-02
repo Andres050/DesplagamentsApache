32608605938c4aaf04710ff53d18d05b886f6c1f
Andres Garcia Bauza <militaxx5@gmail.com>
2021-12-02 20:55:53 +0100

    no

--- formulari.php
+++ formulari.php
@@ -107,7 +107,7 @@ echo "</div></body>";
 $arrayProductes = array();
 include "CapaNegoci/selectProductos.php";
 var_dump(count($arrayProductes));
-var_dump($arrayProductes[count($arrayProductes)]);
+var_dump($arrayProductes[count($arrayProductes)-1]);
 
 if (isset($_GET['name']) && isset($_GET['desc']) && isset($_GET['price']) && isset($_GET['img']))  {
     $producto = new Producto(null, $_GET['name'],$_GET['desc'],$_GET['price'],$_GET['img']);
