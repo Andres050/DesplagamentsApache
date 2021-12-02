33f7c186a7431799e6ac3acd4e8297406fa61024
Andres Garcia Bauza <militaxx5@gmail.com>
2021-12-02 17:58:58 +0100

    no

--- CapaNegoci/selectProductWithID.php
+++ CapaNegoci/selectProductWithID.php
@@ -12,6 +12,7 @@ if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         if ($id == $row["idPro"]) {
+            echo "true";
             $producto = new Producto($row["idPro"],$row["namePro"],$row["descriptPro"],$row["pricePro"],$row["img"]);
             $ArrProductoID = (array)$producto;
             $ArrProductoID = array_values($ArrProductoID);
