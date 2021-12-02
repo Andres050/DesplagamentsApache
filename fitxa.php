0140d78975b1fc97d83c2b32b070d12a31f8a08d
Andres Garcia Bauza <militaxx5@gmail.com>
2021-12-02 17:07:06 +0100

    no

--- fitxa.php
+++ fitxa.php
@@ -83,8 +83,8 @@ include "CapaNegoci/selectTraductions.php";
 
 echo "<div class='productos'><img class='center' src=/img/". $ArrProductoID[4] . ">"; include($_SERVER['DOCUMENT_ROOT'].'/CapaNegoci/selectTraductions.php');
 //echo var_dump($ArrProductoTra);
-echo "<p><strong>".$lang['name']."</strong>". $ArrProductoTra[1] ."</p>";
-echo "<p><strong>".$lang['descrip']."</strong>". $ArrProductoTra[2] ."</p>";
+echo "<p><strong>".$lang['name']."</strong>". $ArrProductoTra[0][1] ."</p>";
+echo "<p><strong>".$lang['descrip']."</strong>". $ArrProductoTra[0][2] ."</p>";
 echo "<p><strong>".$lang['price'].":</strong> ". $ArrProductoID[3] . "</p>";
 echo "<button onclick=\"window.open('/carretaAfegir.php?id=". $ArrProductoID[0]. "', '_blank')\" type='button'>".$lang['BuyPro']."</button></div>";
 
