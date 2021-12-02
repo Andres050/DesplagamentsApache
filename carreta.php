a2bfa8662e6994618932e681a427d84e3424e87f
Andres Garcia Bauza <militaxx5@gmail.com>
2021-12-02 23:26:18 +0100

    no

--- carreta.php
+++ carreta.php
@@ -74,10 +74,10 @@ $id = isset($_GET["id"]) ? $_GET["id"] : 1;
 
 echo "<table><tr><th>". $lang['id'] . "</th><th>". $lang['name']. "</th><th>".$lang['descrip']."</th><th>". $lang['price']."</th><th>".$lang['quantity']."</th><th>". $lang['option']. "</th></tr>";
 $sumatotal = 0;
+var_dump($_SESSION["arrayItems"]);
 
 if (isset($_SESSION["arrayItems"])) {
 	for ($i = 0, $size = count($_SESSION["arrayItems"]); $i < $size; $i++) {
-        var_dump($_SESSION["arrayItems"][$i]);
         echo "<tr><td>" . $_SESSION["arrayItems"][$i]->idPro . "</td>";
         $ArrProductoTra = array();
         include "CapaNegoci/selectTraductions.php";
