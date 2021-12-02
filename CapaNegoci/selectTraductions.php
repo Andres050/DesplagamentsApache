e4e3c79400f2cd1111878dff9e88340d85f86426
Andres Garcia Bauza <militaxx5@gmail.com>
2021-12-02 13:53:17 +0100

    no

--- CapaNegoci/selectTraductions.php
+++ CapaNegoci/selectTraductions.php
@@ -4,7 +4,7 @@ include_once("Producto.php");
 $lenguaje  = $_SESSION['lang'];
 $idPro = $_GET["idPro"];
 
-$resultado = "SELECT * FROM traducionProductos WHERE idPro = $idPro AND nombre_idioma = '$lenguaje'";
+$resultado = "SELECT * FROM traducionProductos WHERE idPro = ". $idPro . " AND nombre_idioma = '$lenguaje'";
 echo $resultado;
 $tra = $conn->query($resultado);
 if ($resultado->num_rows > 0) {
