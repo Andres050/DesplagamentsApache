1000e17ee595ab7b4aefb8030352f7e7b16f840a
Andres Garcia Bauza <militaxx5@gmail.com>
2021-12-02 13:36:19 +0100

    no

--- CapaNegoci/selectTraductions.php
+++ CapaNegoci/selectTraductions.php
@@ -11,6 +11,7 @@ if ($resultado->num_rows > 0) {
     while($row = $resultado->fetch_assoc()) {
         // CREO EL OBJETO PRODUCTO CON EL CONTENIDO DE CADA PRODUCTO!!!
         $producto = array($row["idPro"],$row["namePro"],$row["descripPro"]);
+        $producto = array_values($producto);
         array_push($arrayProductesTraducciones, $producto);
     }
 }
